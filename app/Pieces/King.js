346d5e1bd798dd13e8a952a1a0b218c00b49b3d6
m-chan <mantingchan2@gmail.com>
2021-09-10 14:47:57 +0100

    fixed bug 'null'

--- app/Pieces/King.js
+++ app/Pieces/King.js
@@ -20,28 +20,31 @@ export class King extends Pawn {
         this.castlingQueen = false;
         this.castlingKing = false;
 
-        if ((this.moves === 0) && (!this.check)); { //allows castling if the king has not moved yet and isn't in check
-  
-            //Queen-side castling
-            if ((this.chessBoard[this.oI][3].isEmpty()) && (this.chessBoard[this.oI][2].isEmpty()) && (this.chessBoard[this.oI][1].isEmpty())) { //checks if the squares in-between are empty
-                if (((!this.chessBoard[0][3].threatenedByWhite) && (!this.chessBoard[0][2].threatenedByWhite)) || ((!this.chessBoard[7][3].threatenedByBlack) && (!this.chessBoard[7][2].threatenedByBlack))) { //checks to see if any squares the king moves over isn't threatened 
-                    if (this.chessBoard[this.oI][0].piece.moves === 0) { //if the rook partaking in the castling hasn't moved yet, castle
-                        this.aML.push([this.oI, this.iI-2]);
-                        this.castlingQueen = true;
+        try {
+            if ((this.moves === 0) && (!this.check)); { //allows castling if the king has not moved yet and isn't in check
+    
+                //Queen-side castling
+                if ((this.chessBoard[this.oI][3].isEmpty()) && (this.chessBoard[this.oI][2].isEmpty()) && (this.chessBoard[this.oI][1].isEmpty())) { //checks if the squares in-between are empty
+                    if (((!this.chessBoard[0][3].threatenedByWhite) && (!this.chessBoard[0][2].threatenedByWhite)) || ((!this.chessBoard[7][3].threatenedByBlack) && (!this.chessBoard[7][2].threatenedByBlack))) { //checks to see if any squares the king moves over isn't threatened 
+                        if (this.chessBoard[this.oI][0].piece.moves === 0) { //if the rook partaking in the castling hasn't moved yet, castle
+                            this.aML.push([this.oI, this.iI-2]);
+                            this.castlingQueen = true;
+                        }
                     }
                 }
-            }
 
-            //king-side castling
-            else if ((this.chessBoard[this.oI][5].isEmpty()) && (this.chessBoard[this.oI][6].isEmpty())) { 
-                if (((!this.chessBoard[0][5].threatenedByWhite) && (!this.chessBoard[0][6].threatenedByWhite)) || ((!this.chessBoard[7][5].threatenedByBlack) && (!this.chessBoard[7][6].threatenedByBlack))) { //checks to see if any squares the king moves over isn't threatened  
-                    if (this.chessBoard[this.oI][7].piece.moves === 0) { //if the rook partaking in the castling hasn't moved yet, castle
-                        this.aML.push([this.oI, this.iI+2]);
-                        this.castlingKing = true;
+                //king-side castling
+                else if ((this.chessBoard[this.oI][5].isEmpty()) && (this.chessBoard[this.oI][6].isEmpty())) { 
+                    if (((!this.chessBoard[0][5].threatenedByWhite) && (!this.chessBoard[0][6].threatenedByWhite)) || ((!this.chessBoard[7][5].threatenedByBlack) && (!this.chessBoard[7][6].threatenedByBlack))) { //checks to see if any squares the king moves over isn't threatened  
+                        if (this.chessBoard[this.oI][7].piece.moves === 0) { //if the rook partaking in the castling hasn't moved yet, castle
+                            this.aML.push([this.oI, this.iI+2]);
+                            this.castlingKing = true;
+                        }
                     }
                 }
             }
         }
+        catch (error) {} //gets rid of the weird 'can't read value of null' bug
 
         //console.log("king side", this.chessBoard[this.oI][this.iI].piece.castlingKing, "queen side", this.chessBoard[this.oI][this.iI].piece.castlingQueen)
 
@@ -73,12 +76,12 @@ export class King extends Pawn {
 
 
         if (this.colour === "white") { //white is in check
-            //console.log("white in check");
+            console.log("white in check");
                         
         }
 
         else { //black is in check
-            //console.log("black in check");
+            console.log("black in check");
         }
     }
 
